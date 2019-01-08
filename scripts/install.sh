de8cca17fc251d0963404ac50cbe2f482b31a61c
John Smith <hotzel-schm@online.de>
2019-01-08 19:05:08 +0100

    pip, ansible und git

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,5 +10,23 @@ sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
 
 echo $(date) " - EPEL successfully installed"
 
+echo $(date) " - Install Pip"
+
+#curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+#python get-pip.py
+
+echo $(date) " - Install Ansible"
+
+#pip install ansible
+#pip install boto
+
+echo $(date) " - Checkout Git Repo"
+
+#yum -y install git
+
+#git clone https://github.com/tagliateller/openshift-ansible.git openshift-ansible
+#cd openshift-ansible
+#git checkout release-3.11
+
 echo $(date) " - Script Complete"
 
