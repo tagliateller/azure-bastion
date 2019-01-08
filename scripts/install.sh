d6913604373b148a1b367e2ba2097cf6c81d0e74
John Smith <hotzel-schm@online.de>
2019-01-08 18:19:57 +0100

    Script eingefügt

---
+++ scripts/install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo $(date) " - Starting Script"
+
+# Install EPEL repository
+echo $(date) " - Installing EPEL"
+
+yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
+
+echo $(date) " - EPEL successfully installed"
+
+echo $(date) " - Script Complete"
+
